3e85667c6473f00ad72ddc52876567248f7e3810
Simeon Adesanya <118853003+Simeon-JAA@users.noreply.github.com>
2023-03-09 10:55:44 +0000

    Navbar - dropdown menu update
    Updated JS in alignment with responsive navbar and dropdown menu:
    - to change color when the fixed navbar is toggled
    - to open drop down menu when the icon is clicked
    - to close dropdown menu when the icon is clicked again or an item from the dropdown menu is selected

--- _Script/nav-bar.js
+++ _Script/nav-bar.js
@@ -1,11 +1,35 @@
-//define elements
-let hamburger = document.querySelector('.hamburger');
-let hamburgerButton = document.querySelector('.material-symbols-outlined')
-let btn = document.getElementsByClassName('test')
+const menuToggle = document.querySelector('#menu-toggle');
+const menuBtn = document.querySelector('#menu-btn');
+const menu = document.querySelector('#menu');
+const navbar = document.querySelector('#navbar');
+const listItem = document.querySelectorAll('.list-item');
 
-//hamburger active function
-hamburger.addEventListener('click', () => {
-  hamburgerButton.classList.toggle('hamburger-active');
-  console.log('working')
+// Dropdown Menu Function - Start
+menuToggle.addEventListener('click', () => {
+  menuBtn.classList.toggle('menu-toggle-active');
+  menu.classList.toggle('menu-active');
+  }
+)
+listItem.forEach((link) => {
+  link.addEventListener('click', () => {
+    menuBtn.classList.remove('menu-toggle-active');
+    menu.classList.remove('menu-active');
+  });
+});
+
+
+// Toggle Fixed Navbar
+
+window.addEventListener('scroll', () => {
+  let windowHeight = window.pageYOffset;
+  const navbarHeight = navbar.getBoundingClientRect().height;
+
+  if (windowHeight > navbarHeight) {
+    navbar.classList.add('fixed-nav');
+    menu.style.background = 'var(--grey-trans)'
+
+  } else {
+    navbar.classList.remove('fixed-nav');
+    menu.style.background = 'transparent'
+  }
 })
-
